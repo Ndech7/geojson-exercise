32d40530f39a8e28df2146f0dc5644979799cad2
Ndech7 <stephennderitu3@gmail.com>
2023-08-16 11:31:15 +0300

    fetch the data and implement a styling scheme

--- main.js
+++ main.js
@@ -29,4 +29,35 @@ let basemaps = {
   "ESRI Imagery": esriImagery,
 };
 
+// fetch the data
+//determine quantile breaks(using QGIS)
+let breaks = [0, 3.8, 11.4, 21.9, 52.7, Infinity];
+//define a color scheme for the different break intervals
+let colors = ["#edf8fb", "#b3cde3", "#8c96c6", "#8856a7", "#810f7c"];
+//define a for loop to iterate through the color scheme
+let county_color = (d) => {
+  for (let i = 0; i < breaks.length; i++) {
+    if (d > breaks[i] && d < breaks[i + 1]) {
+      return colors[i];
+    }
+  }
+};
+//define a func to set the styling of the data
+let county_style = (feature) => {
+  return {
+    fillColor: county_color(feature.properties.density),
+    color: "black",
+    opacity: 1,
+    fillOpacity: 0.7,
+    weight: 0.5,
+  };
+};
+fetch("data/county2_2p.geojson")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    L.geoJSON(data, { style: county_style }).addTo(map);
+  });
+
 L.control.layers(basemaps).addTo(map);
